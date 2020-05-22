620680607d191f9d3672c3fef0fcb456d7d4ffd4
BrenoXavier <breno.xavier@upflow.me>
2020-05-21 22:42:55 -0300

    Filter section initialized

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaSearch, FaCaretDown } from 'react-icons/fa'
 import './index.css';
 
 class Filter extends React.Component{
@@ -7,10 +8,39 @@ class Filter extends React.Component{
             <div className="filter__container">
                 <section className="filter__section">
                     <div className="search__filter">
-                        <input type="text" className="filter__input"/>
-                        
+                        <input type="text" className="filter__input" placeholder="Pesquisar..."/>
+                        <FaSearch></FaSearch>
                     </div>
-
+                    <button className={`filter__sort__button ${false ? 'selected' : ''}`}>
+                        <div className="text">
+                            Nome
+                        </div>
+                        <FaCaretDown></FaCaretDown>
+                    </button>
+                    <button className={`filter__sort__button ${true ? 'selected' : ''}`}>
+                        <div className="text">
+                            País
+                        </div>
+                        <FaCaretDown></FaCaretDown>
+                    </button>
+                    <button className={`filter__sort__button ${false ? 'selected' : ''}`}>
+                        <div className="text">
+                            Empresa
+                        </div>
+                        <FaCaretDown></FaCaretDown>
+                    </button>
+                    <button className={`filter__sort__button ${false ? 'selected' : ''}`}>
+                        <div className="text">
+                            Departamento
+                        </div>
+                        <FaCaretDown></FaCaretDown>
+                    </button>
+                    <button className={`filter__sort__button ${true ? 'selected' : ''}`}>
+                        <div className="text">
+                            Data de admissão
+                        </div>
+                        <FaCaretDown></FaCaretDown>
+                    </button>
                 </section>
             </div>
         )
